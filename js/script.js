01a5dba49659334cf61a11b56b8fa48b92106651
jlotten <jenna.otten@gmail.com>
2023-05-05 13:33:19 -0700

    Fetched and displayed titled repos on page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 //variable for github username
 const username = "JLOtten";
+//select the ul to display the repos list
+const repoList = document.querySelector(".repo-list");
 
 const getProfileInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -31,4 +33,23 @@ const displayUserInfo = function(data) {
   </div>`;
   //append this div to the overview element
   overview.append(div);
+  getRepos();
+};
+
+//create an async function to fetch repos
+const getRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    console.log(repoData); 
+repoInfo(repoData); 
+};
+
+//create a function to display info about each repo
+const repoInfo = function (repos) {
+    for (const repo of repos) { //loop throu each repo in the repos
+        const repoItem = document.createElement("li"); //create a list item for each repo
+        repoItem.classList.add("repo"); //give each item a class of repo
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`; //give each item an <h3> with the repo name
+        repoList.append(repoItem); //append the list item to the global variable that grabs the ul repo list
+    }
 };
